1ec65316ae6c47aab3bd23eed5410c5c186aa549
EliasDeHondt <Elias.dehondt@outlook.com>
2024-10-21 22:18:21 +0200

    Refactor script sourcing and update configuration files: Add error handling for no script selected

--- Tools/menu.sh
+++ Tools/menu.sh
@@ -34,6 +34,7 @@ function main() { # Function: Main function.
     local CHOICE=$(dialog --title "Select a Script" --menu "Choose a script to run:" 15 50 "${#MENU_OPTIONS[@]}" "${MENU_OPTIONS[@]}" 3>&1 1>&2 2>&3)
 
     if [ -z "$CHOICE" ]; then error_exit_ui "No script selected."; fi
+    error_exit_ui "$CHOICE"
 
     if [[ "$CHOICE" == *.sh ]]; then bash <(curl -s $BASE_URL/$CHOICE)
     elif [[ "$CHOICE" == *.py ]]; then python3 <(curl -s $BASE_URL/$CHOICE)
