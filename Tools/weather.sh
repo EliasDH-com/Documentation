fbe108bee3fd40ecfee046dbbf7036fe8931d413
EliasDeHondt <Elias.dehondt@outlook.com>
2024-10-21 23:30:18 +0200

    Refactor script sourcing: Update script sourcing method, menu options, and configuration files

--- Tools/weather.sh
+++ Tools/weather.sh
@@ -27,36 +27,50 @@ get_weather() { # Function: Get weather information.
     local CITY_NAME=$1
     local CITY_ID=$2
 
-    # API-aanroep
+    # API call
     local RESPONSE=$(curl -s "$OPENWEATHERMAP_URL_API?id=$CITY_ID&appid=$API_KEY&units=metric")
 
-    # Gegevens extraheren met jq
+    # Extract data using jq
     local WEATHER=$(echo "$RESPONSE" | jq -r '.weather[0].description')
     local TEMPERATURE=$(echo "$RESPONSE" | jq -r '.main.temp')
     local TEMP_MIN=$(echo "$RESPONSE" | jq -r '.main.temp_min')
     local TEMP_MAX=$(echo "$RESPONSE" | jq -r '.main.temp_max')
     local HUMIDITY=$(echo "$RESPONSE" | jq -r '.main.humidity')
 
-    # Weerbericht weergeven
-    dialog --msgbox "Weer in $CITY_NAME:\nBeschrijving: $WEATHER\nTemperatuur: ${TEMPERATURE}°C\nMinimale temperatuur: ${TEMP_MIN}°C\nMaximale temperatuur: ${TEMP_MAX}°C\nVochtigheid: ${HUMIDITY}%" 15 60
+    # Display weather report
+    dialog --msgbox "Weather in $CITY_NAME:\nDescription: $WEATHER\nTemperature: ${TEMPERATURE}°C\nMinimum Temperature: ${TEMP_MIN}°C\nMaximum Temperature: ${TEMP_MAX}°C\nHumidity: ${HUMIDITY}%" 15 60
 }
 
 main() { # Function: Main function.
     check_privileges
     check_dependencies "dialog" "curl" "gpg" "jq"
 
-    local MENU_OPTIONS=()
-    for city in "${!locations[@]}"; do
-        MENU_OPTIONS+=("$city" "$city")
+    local CHOICE
+    while true; do
+        local MENU_OPTIONS=()
+        for city in "${!locations[@]}"; do
+            MENU_OPTIONS+=("$city" "$city")
+        done
+
+        # Add option to go back to menu
+        MENU_OPTIONS+=("Back" "Select a different location")
+
+        CHOICE=$(dialog --title "Weather Locations" --menu "Choose a location to get the weather:" 15 50 ${#MENU_OPTIONS[@]} "${MENU_OPTIONS[@]}" 3>&1 1>&2 2>&3)
+
+        if [ $? -ne 0 ]; then exit 1; fi  # Exit on dialog cancel
+
+        if [ "$CHOICE" == "Back" ]; then
+            clear
+            continue  # Go back to the menu
+        fi
+
+        if [ -z "$CHOICE" ]; then error_exit_ui "No location selected."; fi
+
+        get_weather "$CHOICE" "${locations[$CHOICE]}"
+        clear
     done
 
-    local CHOICE=$(dialog --title "Weather Locations" --menu "Choose a location to pick it up again:" 15 50 ${#MENU_OPTIONS[@]} "${MENU_OPTIONS[@]}" 3>&1 1>&2 2>&3 3>&-)
-
-    if [ -z "$CHOICE" ]; then error_exit_ui "No location selected."; fi
-
-    get_weather "$CHOICE" "${locations[$CHOICE]}"
     remove_files "variables.conf" "functions.conf"
-    clear
 }
 
 main
